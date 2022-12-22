884ea4731097fe5e3b5a8186e469ab7554380939
Erick John Arcena <erickjohn.arcena2@gmail.com>
2022-12-23 00:00:57 +0800

    first commit

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const request = require("request")
 const cities = require('philippines/cities')
-
+const town = require('philippines/provinces')
 const app = express()
 
 app.use('/src', express.static('src'))
@@ -33,7 +33,7 @@ app.get('/cities', (req, res) =>{
         }
     }
     results.results = cities.slice(startIndex, endIndex)
-    res.json(results);
+    res.json(results)
 })
 
-app.listen(3000, () => console.log("Server started on port 3000"))
+app.listen(3000, () => console.log("Server started on port 3000"));
