cb881f1134d02b4d97bf9a7c6b48a2f49737ec0c
Erick John Arcena <erickjohn.arcena@gmail.com>
2022-12-24 03:00:35 +0800

    weather app version 2
    can search up to 5 provinces

--- weather.js
+++ weather.js
@@ -1,38 +1,53 @@
 const express = require("express")
-const request = require("request")
 const cities = require('philippines/cities')
+const where = require('lodash');
 const app = express()
+const apiKey = "635400bf4f54c6cd43cff06a780df130";
 
+app.set('view engine' , 'ejs')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/src', express.static('src'))
 app.use('/node_modules/bootstrap/dist/css', express.static('node_modules/bootstrap/dist/css'))
-app.get("/",(req,res) => {
-    res.sendFile('./index.html', {root: __dirname});
+
+app.get("/", (req,res) => {
+   const weatherUpdate = {location: "Location", temp: "Temp", desc: "Description", humidity: "Humidity", wind:"Wind"}
+   res.render('index', {weatherUpdate: weatherUpdate})
+
 });
 
-app.get('/cities', (req, res) =>{
-    const page = parseInt(req.query.page)
-    const limit = parseInt(req.query.limit)
+app.post("/", async (req,res) => {
 
-    const startIndex = (page - 1) * limit
-    const endIndex = page * limit
 
-    const results = {}
+    let province = await req.body.province
 
-    if(endIndex < cities.length){
-        results.next = { 
-            page: page + 1,
-            limit: limit
-        }
+    let filteredCities = where.filter(cities, 
+        { 'province': province}
+    );
+    const weatherUpdate = [
+       
+    ]
+    for (const index in filteredCities) {  
+
+        let location = await filteredCities[index].name
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`
+        const response = await fetch(apiUrl)
+        const weatherData = await response.json()
+        const temp = Math.floor(weatherData.main.temp)
+        const desc = weatherData.weather[0].description;
+        const humidity = weatherData.main.humidity;
+        const wind = weatherData.wind.speed;
+
+        weatherUpdate.location = location;
+        weatherUpdate.temp = temp;
+        weatherUpdate.desc = desc;
+        weatherUpdate.humidity = humidity;
+        weatherUpdate.wind = wind;
+
+        weatherUpdate.push({location: location, temp: temp, desc:desc, humidity: humidity, wind: wind})
     }
-   
-    if (startIndex > 0){
-        results.previous = { 
-            page: page - 1,
-            limit: limit
-        }
-    }
-    results.results = cities.slice(startIndex, endIndex)
-    res.json(results)
-})
+    res.render('index', {weatherUpdate: weatherUpdate})
+});
+
 
 app.listen(3000, () => console.log("Server started on port 3000"));
