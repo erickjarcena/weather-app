e4688eec4e6b30134e69a5db951d9acceef10fc6
Erick John Arcena <erickjohn.arcena@gmail.com>
2023-04-13 10:18:24 +0800

    test: test commit

--- save.js
+++ save.js
@@ -1,15 +1,4 @@
-const express = require("express")
-const request = require("request")
-const cities = require('philippines/cities')
-const app = express()
-
-app.use('/src', express.static('src'))
-app.use('/node_modules/bootstrap/dist/css', express.static('node_modules/bootstrap/dist/css'))
-app.get("/",(req,res) => {
-    res.sendFile('./index.html', {root: __dirname});
-});
-
-app.get('/cities', (req, res) =>{
+/*app.get('/cities', (req, res) =>{
     const page = parseInt(req.query.page)
     const limit = parseInt(req.query.limit)
 
@@ -33,6 +22,12 @@ app.get('/cities', (req, res) =>{
     }
     results.results = cities.slice(startIndex, endIndex)
     res.json(results)
-})
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+    
+})*/
+/*let filteredCities = where.filter(cities, 
+    { 'province': 'MM'}
+);
+  
+console.log(filteredCities);
+*/
